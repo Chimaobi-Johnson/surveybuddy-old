fca671bb6391228b20cbf4c10b19e7dfeebb9ace
Wogu Chimaobi <woguchimaobi@gmail.com>
2019-09-04 16:54:45 -0700

    added authentication interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TopNav from './containers/Navigation/TopNav';
 import Landing from './components/Landing/Landing';
 import AuthModal from './containers/Authentication/AuthModal';
+import HomeBody from './containers/HomeBody/HomeBody';
+import Dashboard from './Pages/Dashboard';
+import { connect } from 'react-redux';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import * as actions from './store/actions';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  componentDidMount () {
+      this.props.authenticateUser()
+  }
+
+ render() {
+   let routes = (
+    <Switch>
+       <Route path="/surveys" component={Dashboard} />
+       <Route path="/" exact component={Landing}/>
+       <Redirect to="/" />
+    </Switch>
+   )
   return (
     <div className="App">
         <TopNav />
-        <Landing />
-        <AuthModal />      
+        {routes}
     </div>
 
      // pass authreducer from here starting from AuthModal down to TopNav and the rest of the page
      // two reducers authreducer and users reducers for a start
 
   );
+
 }
 
-export default App;
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    authenticateUser: () => dispatch(actions.fetchUser())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(App);
